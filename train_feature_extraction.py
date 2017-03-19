2c737840b49b1be1c1f6d9de4ebd70f0941e1c4b
SherylHohman <SherylHohman@users.noreply.github.com>
2017-03-19 11:31:29 -0700

    improvements based on solution code, re-organize
    ** note: truncating dataset for initial testing **
    -  re-organize code
    -  change several variable names
    -  use tf.nn.sparse_softmax_cross_entropy_with_logits
          which combines: softmax + cross_entropy + one-hot
    -  change bias tf.Variable from tf.int32 to tf.int64, kept weights as tf.float32
          not sure why they think int32 is too small. not taking chances!
    -  add feedback on time it takes to train each epoch :-)

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -1,31 +1,21 @@
+import time
 import pickle
 import tensorflow as tf
 from   sklearn.model_selection import train_test_split
 from   sklearn.utils import shuffle
 from   alexnet import AlexNet
 
-# TODO: Load traffic signs data.
+#set training values
+mu = 0              #??
+sigma = 0.1         #??  #try .01 - solution uses .01
+rate  = 0.001       #??
+EPOCHS = 10         #??
+# temp testing
+EPOCHS = 4
+print("EPOCHS: ", EPOCHS)
+BATCH_SIZE = 128    #??
 
-print("opening")
-with open('train.p', 'rb') as f:
-  data = pickle.load(f)
-#see how our data is stored, so we can know how to extract  X and y from it
-#print(data)
-# ok, data is a dictionary, with 'features', 'coords, 'labels, 'sizes'
-X = data['features']
-y = data['labels']
-
-print("data size X,y: ", len(X), len(y))
-#temp for testing
-#X = X[0:500]
-#y = y[0:500]
-#print("truncating to ", len(X), "data items")
-
-# TODO: Split data into training and validation sets.
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)  #random_state is seed for random sampling
-
-# TODO: Define placeholders and resize operation.
-# get number of classes
+# read number of classes from signnames.csv
 def bufcount(filename):
     #http://stackoverflow.com/a/850962/5411817
     f = open(filename)
@@ -39,29 +29,41 @@ def bufcount(filename):
         buf = read_f(buf_size)
 
     return lines
-#num_classes = 43  ###?? # len of signnames.csv-2
-num_classes = bufcount('./signnames.csv')
+num_classes = bufcount('./signnames.csv') - 1
 print(num_classes, "classes")
+#num_classes = 43  # len of 'signnames.csv'
 
-print(y.shape, "shape of y")
+# TODO: Load traffic signs data.
 
+print("opening")
+with open('train.p', 'rb') as f:
+  data = pickle.load(f)
+#see how our data is stored, so we know how to extract  X and y from it
+#print(data)
+# findings: data is a dictionary: 'features', 'coords, 'labels, 'sizes'
+X = data['features']
+y = data['labels']
+
+print("data size X, y: ", len(X), len(y))
+#temp truncate data for initial testing
+X = X[0:400]
+y = y[0:400]
+print("truncating to ", len(X), "data items")
+
+# TODO: Split data into training and validation sets.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)  #random_state is seed for random sampling
+
+# TODO: Define placeholders and resize operation.
 x_shape = (None,) + X_train.shape[1:]
 #print("x_shape", x_shape)
 
-x = tf.placeholder(tf.float32, x_shape)
-y = tf.placeholder(tf.int32, None)
-one_hot_y = tf.one_hot(y, num_classes)
-
-mu = 0              #??
-sigma = 0.1         #??
+features = tf.placeholder(tf.float32, x_shape)
+labels   = tf.placeholder(tf.int64, None)
+#one_hot_y = tf.one_hot(y, num_classes)
 
 # Alexnet requires 227px x 227px images
-resized = tf.image.resize_images(x, (227,227))
+resized = tf.image.resize_images(features, (227,227))
 print("resized")
-
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
 
 # TODO: pass placeholder as first argument to `AlexNet`.
 fc7 = AlexNet(resized, feature_extract=True)
@@ -74,92 +76,95 @@ fc7 = tf.stop_gradient(fc7)
 # TODO: Add the final layer for traffic sign classification.
 shape = (fc7.get_shape().as_list()[-1], num_classes)
 fc8_W  = tf.Variable(tf.truncated_normal(shape, mean=mu, stddev=sigma))
-fc8_b  = tf.zeros(num_classes)
-logits = tf.matmul(fc7, fc8_W) + fc8_b
-logits = tf.nn.softmax(logits)
+fc8_b  = tf.Variable(tf.zeros(num_classes))
+logits = tf.matmul(fc7, fc8_W) + fc8_b  # or tf.nn.xw_plus_b(fc7, fc8W, fc8b)
+# see notes below as to why not using softmax directly
+#logits = tf.nn.softmax(logits)
 print("got logits")
 
 # TODO: Define loss, training, accuracy operations.
 # HINT: Look back at your traffic signs project solution, you may
 # be able to reuse some the code.
 
-# training pipeline:
-rate = 0.001        #??
-EPOCHS = 10         #??
-BATCH_SIZE = 128    #??
-
-# temp testing
-#EPOCHS = 4
-#print("EPOCHS: ", EPOCHS)
-
-
 # loss
-cross_entropy  = tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y)
+#   http://stackoverflow.com/a/34243720/5411817
+#   use this function instead of separate functions:
+#   1) softmax with 2) cross_entropy and 3)(sparce) includes one-hot
+#   softmax_cross_entropy_with_logits is more numerically stable/
+#       accurate than running two steps of softmax, then cross_entropy
+#   using the sparse_.. saves a step by not having to convert labels
+#       to one-hot first
+cross_entropy  = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels)
 loss_operation = tf.reduce_mean(cross_entropy)
+
 # training
 optimizer = tf.train.AdamOptimizer(learning_rate = rate)
 training_operation = optimizer.minimize(loss_operation)
+
 # accuracy
+model_prediction = tf.argmax(logits, 1)
+prediction_is_correct = tf.equal(model_prediction, labels)
+accuracy_operation = tf.reduce_mean(tf.cast(prediction_is_correct, tf.float32))
 
-# TODO: Train and evaluate the feature extraction model.
-
-# define evaluation routines
-correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
-accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# save trained model
 saver = tf.train.Saver()
 
 
+# TODO: Train and evaluate the feature extraction model.
+
+# define evaluation routine
 def evaluate(X_data, y_data):
   print("evaluating..")
-  num_examples = len(X_data)
-  total_accuracy = 0
   sess = tf.get_default_session()
+  total_accuracy = 0
+  total_loss = 0
 
+  num_examples = X_data.shape[0]     #len(X_data)
   for offset in range(0, num_examples, BATCH_SIZE):
-      batch_x = X_data[offset:offset+BATCH_SIZE]
-      batch_y = y_data[offset:offset+BATCH_SIZE]
-      accuracy = sess.run(accuracy_operation,
-                          feed_dict={x: batch_x, y: batch_y})
-      total_accuracy += (accuracy * len(batch_x))
+      X_batch = X_data[offset:offset+BATCH_SIZE]
+      y_batch  = y_data[offset:offset+BATCH_SIZE]
 
-  return total_accuracy / num_examples
+      accuracy, loss = sess.run([accuracy_operation,
+                                loss_operation],
+                                feed_dict={features: X_batch,
+                                           labels:   y_batch})
+      batch_size = X_batch.shape[0]   #len(X_batch)
+      total_accuracy += (accuracy * batch_size)
+      total_loss     += (loss * batch_size)
+
+  return total_accuracy/num_examples, total_loss/num_examples
 
 # train the model
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    num_examples = len(X_train)
-    #temp testing
-    #num_examples = 100
-    print("training", num_examples, "examples" )
 
     print("Training...")
     print()
 
+    num_examples = len(X_train) # == X_train.shape[0]
+    #temp testing
+    print(X_train.shape[0], "=?=", num_examples)
+    print("training", num_examples, "examples, in", EPOCHS, "EPOCHS" )
+
     for i in range(EPOCHS):
       X_train, y_train = shuffle(X_train, y_train)
+      t0 = time.time()
       for offset in range (0, num_examples, BATCH_SIZE):
-          print("     batch ", 1+offset//BATCH_SIZE, "of ", 1+ num_examples/BATCH_SIZE)
+          print("     batch ", 1+offset//BATCH_SIZE, "of ", 1 + num_examples/BATCH_SIZE, "batches,  on EPOCH", i+1, "of", EPOCHS, "EPOCHS")
           end = offset + BATCH_SIZE
-          batch_x = X_train[offset:end]
-          batch_y = y_train[offset:end]
           sess.run(training_operation,
-                   feed_dict={x: batch_x, y: batch_y})
+                   feed_dict={features: X_train[offset:end],
+                              labels:   y_train[offset:end]})
 
-        # evaluate the model
-      validation_accuracy = evaluate(X_test, y_test)
+      # evaluate the model, print results
+      # ??? on testing set ?? This is within training loop !
+      validation_accuracy, validation_loss = evaluate(X_test, y_test)
       print("EPOCH {} ...".format(i+1))
+      print("Time: {:.3f} minutes".format( float((time.time()-t0)/60)) )
       print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+      print("Validation Loss = {:.3f}".format(validation_loss))
       print()
 
     # save trained model
     saver.save(sess, './sh-trained')
     print("Model saved")
-
-
-# evaluate model on test set
-with tf.Session() as sess:
-  saver.restore(sess, tf.train.latest_checkpoint('.'))
-
-  test_accuracy = evaluate(X_test, y_test)
-  print("Test Accuracy = {:.3f}".format(test_accuracy))
-
